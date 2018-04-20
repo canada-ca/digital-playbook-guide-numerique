fa767bba62f94f220484de917021a481891b28ca
Nick Schonning <nschonni@gmail.com>
2018-04-20 12:32:53 -0400

    chore: Add link checking (#62)

---
+++ link-check.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var markdownLinkCheck = require('markdown-link-check');
+var fs = require("fs");
+var glob = require("glob");
+var path = require("path");
+var url = require("url");
+var chalk = require("chalk");
+
+var internalGCHosts = ["www.gcpedia.gc.ca", "wiki.ic.gc.ca"]
+var files = glob.sync("**/*.md", {ignore: "node_modules/**/*.md"})
+
+var deads = false;
+
+files.forEach(function(file) {
+  var markdown = fs.readFileSync(file).toString();
+  var opts = {};
+  opts.baseUrl = 'file://' + path.dirname(path.resolve(file));
+
+  markdownLinkCheck(markdown, opts, function (err, results) {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+
+    console.log(chalk.green("Reading: " + file));
+
+    results.forEach(function (result) {
+      if(result.status === "dead") {
+        var target = url.parse(result.link);
+
+        if(internalGCHosts.indexOf(target.hostname) == -1)
+        {
+          deads = true;
+          console.log(chalk.red("Dead: " + result.link));
+        }
+        else {
+          console.log(chalk.yellow("Internal GC link: " + result.link));
+        }
+      }
+    });
+    if(deads) {
+      console.error(chalk.red('\nERROR: dead links found!'));
+      process.exit(1);
+    }
+  });
+});
