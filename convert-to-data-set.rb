cc2f2d323fa057a60d20eedba4047fb466b8f782
pjackson28 <Paul.Jackson@tbs-sct.gc.ca>
2018-05-18 04:26:13 -0400

    Fixed handling of sections without section elements

--- convert-to-data-set.rb
+++ convert-to-data-set.rb
@@ -29,7 +29,9 @@ def build_content_array(html_block, is_parent, parent_heading_level)
 		elems = html_block
 	end
 	elems.each do |elem|
-		unless ignore_loop > 0 || elem.name === "h#{parent_heading_level}"
+		if ignore_loop > 0
+			ignore_loop -= 1 
+		elsif elem.name != "h#{parent_heading_level}"
 			item = Hash.new
 			elem_class = elem["class"]
 			if elem_class.nil? || elem_class.length == 0
@@ -59,12 +61,11 @@ def build_content_array(html_block, is_parent, parent_heading_level)
 				item["title"] = elem.inner_html
 				node_array = Array.new
 				next_elem = elem.next_element
-				ignore_loop += 1
 				while !next_elem.nil? && next_elem.name != "h#{parent_heading_level + 1}"
 					node_array.push(next_elem)
 					next_elem = next_elem.next_element
-					ignore_loop += 1
 				end
+				ignore_loop += node_array.count
 				item["content"] = build_content_array(node_array, false, parent_heading_level + 1)
 			elsif elem.name === "ul" || elem.name === "ol"
 				item["type"] = "list"
@@ -93,10 +94,6 @@ def build_content_array(html_block, is_parent, parent_heading_level)
 				item["content"] = elem
 			end
 			content_array.push(item)
-		end
-
-		if ignore_loop > 0
-			ignore_loop -= 1
 		end
 	end
 	return content_array
