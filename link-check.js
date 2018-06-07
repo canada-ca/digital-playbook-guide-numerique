5ad313cbf0010072f47097ff9d1ce32bf9f3bfbd
Nick Schonning <nschonni@gmail.com>
2018-06-06 23:02:58 -0400

    Markdowdow link check ignore patterns (#189)
    * fix: Dev stage checklist link
    
    * chore: use Markdown-link-check pattern properties

--- link-check.js
+++ link-check.js
@@ -9,15 +9,16 @@ var path = require("path");
 var url = require("url");
 var chalk = require("chalk");
 
-var internalGCHosts = ["www.gcpedia.gc.ca", "intranet.canada.ca"]
 var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md", "**/digital-playbook.md", "**/guide-numerique.md"]})
 
 var deads = false;
 
+var opts = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
+
+opts.baseUrl = "file://" + path.resolve(__dirname, "_site/").replace(/\\/g, '/') + "/"
+
 files.forEach(function(file) {
   var markdown = fs.readFileSync(file).toString();
-  var opts = {};
-  opts.baseUrl = 'file://' + path.dirname(path.resolve(file));
 
   markdownLinkCheck(markdown, opts, function (err, results) {
     if (err) {
@@ -29,17 +30,12 @@ files.forEach(function(file) {
 
     results.forEach(function (result) {
       if(result.status === "dead") {
-        var target = url.parse(result.link);
-
-        if(internalGCHosts.indexOf(target.hostname) == -1)
-        {
-          deads = true;
-          console.log(chalk.red("Dead: " + result.link));
-        } else if (result.statusCode == 500) {
+        if (result.statusCode == 500) {
           console.log(chalk.yellow("Server error on target: " + result.link));
         }
         else {
-          console.log(chalk.yellow("Internal GC link: " + result.link));
+          deads = true;
+          console.log(chalk.red("Dead: " + result.link));
         }
       }
     });
