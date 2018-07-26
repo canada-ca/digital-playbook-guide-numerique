bb3780b20c3a477bac4c5dd984692e20b8f576ea
pjackson28 <Paul.Jackson@tbs-sct.gc.ca>
2018-07-26 01:10:00 -0400

    Cleaned up format-gen.js

--- assets-atouts/js/format-gen.js
+++ assets-atouts/js/format-gen.js
@@ -1,6 +1,6 @@
 /**
- * @title Format generator
- * @overview Generates a different format (e.g., CSV) using content on the page
+ * @title Format download generator
+ * @overview Generates a download in a different format (e.g., CSV) using content on the page
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  * @author @pjackson28
  */
@@ -17,6 +17,9 @@ var componentName = "wb-format-gen",
     selector = "." + componentName,
     initEvent = "wb-init" + selector,
     $document = wb.doc,
+    defaults = {
+      filename: "data"
+    },
 
     /**
      * @method init
@@ -28,29 +31,7 @@ var componentName = "wb-format-gen",
       // Start initialization
       // returns DOM object = proceed with init
       // returns undefined = do not proceed with init (e.g., already initialized)
-      var elm = wb.init( event, componentName, selector );
-
-      if ( elm ) {
-
-        var $elm = $( elm ),
-            settings = {
-              ignoreInit: false
-            }, $eventElement, type, linkSelector;
-
-        // Extend the settings with window[ "wb-format-gen" ] then data-wb-format-gen
-        settings = $.extend(
-          true,
-          settings,
-          window[ componentName ],
-          wb.getData( $elm, componentName )
-        );
-
-        type = settings[ "type" ];
-        linkSelector = settings[ "linkSelector" ];
-
-        if ( type === "csv" ) {
-          $( linkSelector).attr( "href", encodeURI( toCSV( currentSettings[ "rowSelector" ], currentSettings[ "colSelector" ], currentSettings[ "container" ] ) ) );        }
-      }
+      wb.init( event, componentName, selector );
     },
 
     /**
@@ -73,7 +54,7 @@ var componentName = "wb-format-gen",
         numCols = $cols.length;
         for ( colIndex = 0; colIndex < numCols; colIndex += 1 ) {
           colContent = $cols.eq( colIndex ).text().replace( /"/g, '""' );
-          if ( colContent.index( "," ) != -1 ) {
+          if ( colContent.indexOf( "," ) != -1 ) {
             colContent = '"' + colContent + '"';
           }
           rowOutput.push( colContent );
@@ -82,7 +63,94 @@ var componentName = "wb-format-gen",
       }
 
       return output.join( "\n" );
+    },
+
+    /**
+     * @method toJSON (to build)
+     * @param ?
+     * @return {String} JSON output
+     */
+    toJSON = function() {
+      return;
+    },
+
+    /**
+     * @method toXML (to build)
+     * @param ?
+     * @return {String} XML output
+     */
+    toXML = function() {
+      return;
+    },
+
+    /**
+     * @method outputFile
+     * @param fileData {String} String containing the data for the file
+     * @param mimeType {String} MIME type of the output file (e.g., "text/csv;charset=utf-8;")
+     * @param filename {String} Filename for the file to output (excluding file extension)
+     * @return {String} CSV output
+     */
+    outputFile = function( fileData, mimeType, filename ) {
+      var blobOutput, urlOutput, outputLink;
+      try {
+        blobOutput = new Blob( [ fileData ], { type: mimeType } );
+
+        try {
+          urlOutput = URL.createObjectURL( blobOutput );
+        } catch ( e ) {
+          if (navigator.msSaveBlob) {
+            // Backwards compatibility for IE10
+            navigator.msSaveBlob( blobOutput, filename );
+            return;
+          }
+          // Trigger the fallback approach
+          throw "Use fallback";
+        }
+      } catch ( e ) {
+        // Fallback for where Blob support doesn't exist
+        urlOutput = encodeURI( "data:" + mimeType.slice( 0, -1 ) + "," + fileData );
+      }
+
+      outputLink = document.createElement( "a" );
+      if ( outputLink.download !== undefined ) {
+        outputLink.setAttribute( "href", urlOutput );
+        outputLink.setAttribute( "download", filename );
+        outputLink.style.visibility = "hidden";
+        document.body.appendChild( outputLink );
+        outputLink.click();
+        document.body.removeChild( outputLink );
+      } else {
+        // Last resort fallback
+        window.open( urlOutput );
+      }
     };
+
+
+$document.on( "click", selector, function( event ) {
+  // Extend the settings with window[ "wb-format-gen" ] then data-wb-format-gen
+  var settings = $.extend(
+    true,
+    defaults,
+    window[ componentName ],
+    wb.getData( $( event.target ), componentName )
+  ),
+  type = settings[ "type" ].toLowerCase(),
+  filename = settings[ "filename" ],
+  fileData, mimeType;
+
+  if ( type === "csv" ) {
+    fileData = toCSV( settings[ "rowSelector" ], settings[ "colSelector" ], settings[ "container" ] );
+    mimeType = "text/csv;charset=utf-8;"
+  } else if ( type === "json" ) {
+    fileData = toJSON(); // To build
+    mimeType = "application/json;charset=utf-8;"
+  } else if ( type === "xml" ) {
+    fileData = toXML(); // To build
+    mimeType = "application/xml;charset=utf-8;"
+  }
+
+  outputFile( fileData, mimeType, filename );
+} );
 
 // Bind the init event of the plugin
 $document.on( "timerpoke.wb " + initEvent, selector, init );
