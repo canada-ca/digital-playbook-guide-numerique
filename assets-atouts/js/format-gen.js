f5126f80e0be840d48c5c2f5951f66f7fd3501e8
pjackson28 <Paul.Jackson@tbs-sct.gc.ca>
2018-07-31 01:20:51 -0400

    Added ability to generate JSON from HTML and output to a file

--- assets-atouts/js/format-gen.js
+++ assets-atouts/js/format-gen.js
@@ -54,45 +54,177 @@ var componentName = "wb-format-gen",
      * @method htmlToCSV
      * @param rowSelector {String} CSS selector for the rows (e.g., "tr:not(.hidden)")
      * @param colSelector {String} CSS selector for the cells within the row (e.g., "td:not(.hidden)")
-     * @param container {String} (Optional, defaults to document) CSS selector for the container to look in (e.g., "#table-id")
-     * @return {String} CSV output
+     * @param container {String} CSS selector for the container to look in (e.g., "#table-id")
+     * @param outputAsString {Boolean} (Optional, defaults to false) Controls the returned value. false = return array, true = return string.
+     * @return {String/Array} CSV output
      */
-    htmlToCSV = function( rowSelector, colSelector, container ) {
+    htmlToCSV = function( rowSelector, colSelector, container, outputAsString ) {
       var output = [],
-          $source = container ? $( container ) : wb.doc,
-          $rows = $source.find( rowSelector ),
-          numRows = $rows.length,
-          rowIndex, $cols, numCols, colIndex, colContent, rowOutput;
+          source = document.querySelector( container ),
+          rows = source.querySelectorAll( rowSelector ),
+          numRows = rows.length,
+          rowIndex, cols, numCols, colIndex, colContent, rowOutput;
 
       for ( rowIndex = 0; rowIndex < numRows; rowIndex += 1 ) {
         rowOutput = [];
-        $cols = $rows.eq( rowIndex ).find( colSelector );
-        numCols = $cols.length;
+        cols = rows[ rowIndex ].querySelectorAll( colSelector );
+        numCols = cols.length;
         for ( colIndex = 0; colIndex < numCols; colIndex += 1 ) {
-          colContent = $cols.eq( colIndex ).text().replace( /"/g, '""' );
+          colContent = cols[ colIndex ].textContent.replace( /"/g, '""' );
           if ( colContent.indexOf( "," ) != -1 ) {
             colContent = '"' + colContent + '"';
           }
           rowOutput.push( colContent );
         }
-        output.push( rowOutput.join( "," ) );
+        output.push( outputAsString === true ? rowOutput.join( "," ) : rowOutput );
       }
 
-      return output.join( "\n" );
+      return outputAsString === true ? output.join( "\n" ) : output;
+    },
+
+    /**
+     * @method htmlToJSON
+     * @param container {HTML node} Container of the data to output to JSON
+     * @param structure {String} Defines the structure of the JSON to output. The following is an example: 
+     * {
+     *   "type": "array",
+     *   "selector": "input",
+     *   "items": {
+     *     "type": "object",
+     *     "properties": {
+     *       "id": {
+     *         "type": "string",
+     *         "valueSource": "id"
+     *       },
+     *       "checked": {
+     *         "type": "boolean",
+     *         "valueSource": "checked"
+     *       }
+     *     }
+     *   }
+     * }
+     * @param outputAsString {Boolean} (Optional, defaults to false) Controls the returned value. false = return object, true = return string.
+     * @return {String/Object} JSON output
+     */
+    htmlToJSON = function( container, structure, outputAsString ) {
+      var selector = structure.selector, 
+          nodes, length, node, items, properties, property, index, itemType, subContainer, valueSource, key, value;
+
+      if ( structure.type.toLowerCase() === "array" ) {
+        nodes = container.querySelectorAll( selector );
+        length = nodes.length;
+        items = structure.items;
+        itemType = items.type.toLowerCase();
+        subContainer = [];
+
+        if ( itemType === "array" || itemType === "object" ) {
+          for ( index = 0; index < length; index += 1 ) {
+            subContainer.push( htmlToJSON( nodes[ index ], items ) );
+          }
+        } else {
+          // String, number or boolean
+          valueSource = structure.valueSource;
+          for ( index = 0; index < length; index += 1 ) {
+            node = nodes[ index ];           
+
+            if ( !valueSource ) {
+              value = node.textContent;
+            } else if ( valueSource in node ) {
+              value = node[ valueSource ];
+            } else {
+              value = node.getAttribute( valueSource );
+            }
+            subContainer.push( value );
+          }
+        }
+      } else {
+        if ( selector ) {
+          container = container.querySelector( selector );
+        }
+        properties = structure.properties;
+        subContainer = {};
+
+        for ( key in properties ) {
+          if ( properties.hasOwnProperty( key ) ) {
+            property = properties[ key ];
+            itemType = property.type.toLowerCase();
+
+            if ( property.selector ) {
+              node = container.querySelector( property.selector );
+            } else {
+              node = container;
+            }
+
+            if ( itemType === "array" || itemType === "object" ) {
+              subContainer[ key ] = htmlToJSON( node, property );
+            } else {
+              // String, number or boolean
+              valueSource = property.valueSource;
+
+              if ( !valueSource ) {
+                value = node.textContent;
+              } else if ( valueSource === "attributes" ) {
+
+                // Special handling for the attributes property.
+                // It iterates through all the attributes and outputs them.
+                nodes = node.attributes;
+                length = nodes.length;
+
+                for ( index = 0; index < length; index += 1 ) {
+                  node = nodes[ index ];
+                  subContainer[ node.name ] = value;
+                }
+              } else if ( valueSource in node ) {
+
+                // Property is on the node so output the property
+                value = node[ valueSource ];
+              } else {
+
+                // Output the attribute
+                value = node.getAttribute( valueSource );
+              }
+
+              if ( valueSource !== "attributes" ) {
+                if ( itemType === "number" ) {
+                  // Convert a number string to an actual number
+                  if ( value.indexOf( "." ) !== -1 ) {
+                    value = parseFloat( value );
+                  } else {
+                    value = parseInt( value );
+                  }
+                } else if ( itemType === "boolean" && typeof value === "string" ) {
+                  // Convert a boolean string to an actual boolean
+                  value = value.toLowerCase() === "true";
+                }
+                subContainer[ key ] = value;
+              }
+            }
+          }
+        }
+      }
+      return outputAsString ? JSON.stringify( subContainer ) : subContainer;
     },
 
     /**
      * @method outputFile
-     * @param fileData {String} String containing the data for the file
-     * @param mimeType {String} MIME type of the output file (e.g., "text/csv;charset=utf-8;")
-     * @param filename {String} Filename for the file to output (excluding file extension)
-     * @param type {String} Type of file (used for the file extension for the msSaveBlob fallback)
-     * @return {String} CSV output
+     * @param settings {Object} Settings object for the file to output
      */
-    outputFile = function( fileData, mimeType, filename, type ) {
+    outputFile = function( settings ) {
       var outputLink = document.createElement( "a" ),
           isDownloadAttrSupported = outputLink.download !== undefined,
-          blobOutput, urlOutput;
+          type = settings[ "type" ].toLowerCase(),
+          filename = settings[ "filename" ],
+          fileData, mimeType, blobOutput, urlOutput;
+
+      if ( type === "csv" ) {
+        fileData = htmlToCSV( settings[ "rowSelector" ], settings[ "colSelector" ], settings[ "container" ], true );
+        mimeType = "text/csv;charset=utf-8;";
+      } else if ( type === "json" ) {
+        fileData = htmlToJSON( document.querySelector( settings[ "container" ] ), settings[ "structure" ], true );
+        mimeType = "application/json;charset=utf-8;";
+      } else {
+        return;
+      }
 
       try {
         blobOutput = new Blob( [ fileData ], { type: mimeType } );
@@ -117,23 +249,47 @@ var componentName = "wb-format-gen",
       document.body.appendChild( outputLink );
       outputLink.click();
       document.body.removeChild( outputLink );
-    };
+    },
 
+    /**
+     * @method storeData
+     * @param settings {Object} Settings object for the data to save
+     */
+    storeData = function( settings ) {
+      var type = settings[ "type" ].toLowerCase(),
+          dataKey = settings[ "dataKey" ],
+          data;
+
+      if ( type === "csv" ) {
+        data = htmlToCSV( settings[ "rowSelector" ], settings[ "colSelector" ], settings[ "container" ], true );
+      } else if ( type === "json" ) {
+        data = htmlToJSON( document.querySelector( settings[ "container" ] ), settings[ "structure" ], true );
+      } else {
+        return;
+      }
+
+      if ( settings[ "useLocalStorage" ] === true ) {
+        localStorage( dataKey, data );
+      } else {
+        sessionStorage( dataKey, data );
+      }
+    },
+
+    /**
+     * @method retrieveData
+     * @param dataKey {String} Key for retrieving the data
+     * @param useLocalStorage {Boolean} (defaults to false) Whether or not to retrieve the data from localStorage
+     */
+    retrieveData = function( dataKey, useLocalStorage ) {
+      if ( useLocalStorage ) {
+        return localStorage( dataKey );
+      } else {
+        return sessionStorage (dataKey );
+      }
+    }
 
 $document.on( "click", selector, function( event ) {
-  var settings = wb.getData( $( event.target ), componentName ),
-      type = settings[ "type" ].toLowerCase(),
-      filename = settings[ "filename" ],
-      fileData, mimeType;
-
-  if ( type === "csv" ) {
-    fileData = htmlToCSV( settings[ "rowSelector" ], settings[ "colSelector" ], settings[ "container" ] );
-    mimeType = "text/csv;charset=utf-8;"
-  } else {
-    return;
-  }
-
-  outputFile( fileData, mimeType, filename, type );
+  outputFile( wb.getData( $( event.target ), componentName ) );
 } );
 
 // Bind the init event of the plugin
